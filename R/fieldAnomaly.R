1b720464ae962dbb3ed1e963de8dd817ec9a5ada
marchtaylor <marchtaylor@gmail.com>
2015-09-30 15:32:53 +0200

    added additional functionality to fieldAnomaly function (month-day filter added)

--- R/fieldAnomaly.R
+++ R/fieldAnomaly.R
@@ -1,26 +1,35 @@
-#' Calculate the daily or monthly anomaly of a field
+#' Calculate the daily (julian or month/day date) or monthly anomaly of a data field
 #' 
-#' The \code{fieldAnomaly} function calculates an anomaly field by substraction the daily or 
+#' The \code{fieldAnomaly} function calculates an anomaly field by subtracting the daily or 
 #' monthly means from each spatial location (columns) of a field.
 #' 
-#' @param x A vector of the class "POSIXlt" containing the dates that correspond
+#' @param x A vector of the class "Date", "POSIXct", or "POSIXlt" containing the dates that correspond
 #' to the rows of matrix \code{y}.
 #' @param y A matrix of the spatio-temporal field with rows being the temporal 
 #' dimension and columns being the spatial dimension
-#' @param level Character string. Anomaly to compute ("daily" or "monthly")
+#' @param level Character string. Anomaly to compute (\code{"julian"}, \code{"day"} or 
+#' \code{"month"}). 
+#' \code{"julian"} will consider unique \code{as.POSIXlt(x)$yday} values, while \code{"day"} will 
+#' consider unique \code{format(x,"\%m\%d")} strings.
 #' 
 #' @examples
+#' ### create synthetic data (contains annual and day signal)
 #' set.seed(1)
-#' Time <- seq.Date(as.Date("1990-01-01"), as.Date("1999-12-01"), by="month")
-#' Space <- seq(0,pi,,30)
-#' Signal <- sin(as.POSIXlt(Time)$mon/2)
-#' Z <- outer(Signal, sin(Space))
-#' Noise <- Z * 0.1 * array(rnorm(length(Z)), dim=dim(Z))
+#' Time <- seq.Date(as.Date("1990-01-01"), as.Date("1996-12-31"), by="day")
+#' Space <- seq(pi,2*pi,,10)
+#' Signal1 <- sin(as.POSIXlt(Time)$mon/11*(2*pi)) # annual signal
+#' Signal2 <- sin(as.POSIXlt(Time)$yday/365*(4*pi)) # quarterly signal
+#' cumSignal <- 1*Signal1+0.3*Signal2
+#' plot(Time, cumSignal, t="l")
+#' abline(v=seq(as.Date("1900-01-01"), as.Date("2100-01-01"), by="year"), lty=2, col=8)
+#' Z <- outer(cumSignal, sin(Space))
+#' Noise <- Z * 0 * array(rnorm(length(Z)), dim=dim(Z))
 #' Z <- Z + Noise
-#' Z.anom <- fieldAnomaly(y=Z, x=as.POSIXlt(Time), level="monthly")
 #' 
+#' # anomaly by month (removes "Signal1")
+#' Z.anom <- fieldAnomaly(y=Z, x=Time, level="month")
 #' zran <- c(-1,1) * max(abs(range(Z, Z.anom)))
-#' pal <- colorRampPalette(c("blue", "cyan", "grey", "yellow", "red"))
+#' pal <- colorPalette(c("blue4", "green", "white", "orange", "red4"), c(10,1,1,10))
 #' op <- par(no.readonly=TRUE)
 #' layout(matrix(c(1,2,3,3), nrow=2, ncol=2), widths=c(4,1), heights=c(2,2))
 #' par(mar=c(5,5,3,1))
@@ -31,27 +40,95 @@
 #' mtext("Value", side=4, line=3)
 #' par(op)
 #' 
+#' 
+#' # anomaly by day of year (i.e. date, removes "Signal1" and "Signal2")
+#' Z.anom <- fieldAnomaly(y=Z, x=Time, level="day")
+#' zran <- c(-1,1) * max(abs(range(Z, Z.anom)))
+#' pal <- colorPalette(c("blue4", "green", "white", "orange", "red4"), c(10,1,1,10))
+#' op <- par(no.readonly=TRUE)
+#' layout(matrix(c(1,2,3,3), nrow=2, ncol=2), widths=c(4,1), heights=c(2,2))
+#' par(mar=c(5,5,3,1))
+#' image(Time, Space, Z, col=pal(100), zlim=zran, main="Original")
+#' image(Time, Space, Z.anom, col=pal(100), zlim=zran, main="Anomaly")
+#' par(mar=c(5,0,3,5))
+#' imageScale(Z, col=pal(100), zlim=zran, axis.pos=4)
+#' mtext("Value", side=4, line=3)
+#' par(op)
+#' 
+#' 
+#' # anomaly by julian day (day of year, removes "Signal1" and "Signal2")
+#' Z.anom <- fieldAnomaly(y=Z, x=Time, level="julian")
+#' zran <- c(-1,1) * max(abs(range(Z, Z.anom)))
+#' pal <- colorPalette(c("blue4", "green", "white", "orange", "red4"), c(10,1,1,10))
+#' op <- par(no.readonly=TRUE)
+#' layout(matrix(c(1,2,3,3), nrow=2, ncol=2), widths=c(4,1), heights=c(2,2))
+#' par(mar=c(5,5,3,1))
+#' image(Time, Space, Z, col=pal(100), zlim=zran, main="Original")
+#' image(Time, Space, Z.anom, col=pal(100), zlim=zran, main="Anomaly")
+#' par(mar=c(5,0,3,5))
+#' imageScale(Z, col=pal(100), zlim=zran, axis.pos=4)
+#' mtext("Value", side=4, line=3)
+#' par(op)
+#' 
+#' 
+#' # test that anomalies are removed correctly
+#' Zalt <- as.matrix(apply(scale(Z),1,mean))
+#' plot(Time, Zalt, t="l")
+#' 
+#' tmp <- aggregate(Zalt ~ as.POSIXlt(Time)$yday, FUN=mean)
+#' plot(strptime(paste("2000",tmp[,1], sep="-"), format="%Y-%j"), tmp[,2],
+#'      t="o", xlab="", ylab="n values", main="level = 'julian'")
+#' 
+#' tmp <- aggregate(Zalt ~ format(Time, "%m-%d"), FUN=mean)
+#' plot(strptime(paste("2000",tmp[,1], sep="-"), format="%Y-%m-%d"), tmp[,2], 
+#'      t="o", xlab="", ylab="n values", main="level = 'day'")
+#' 
+#' 
+#' Z.anom <- fieldAnomaly(y=Zalt, x=Time, level="month")
+#' sqrt(mean(Z.anom^2))
+#' plot(Time, Z.anom, t="l")
+#' 
+#' Z.anom <- fieldAnomaly(y=Zalt, x=Time, level="day")
+#' sqrt(mean(Z.anom^2))
+#' plot(Time, Z.anom, t="l")
+#' 
+#' # here is an issue!!
+#' Z.anom <- fieldAnomaly(y=Zalt, x=Time, level="julian")
+#' sqrt(mean(Z.anom^2))
+#' plot(Time, Z.anom, t="l")
+#' 
+#' 
 #' @export
 #' 
-fieldAnomaly <- function(y, x, level="daily"){ 
+fieldAnomaly <- function(y, x, level="month"){ 
 
 	y <- as.matrix(y)
 
-	if(level=="monthly"){levs=unique(x$mon)}
-	if(level=="daily"){levs=unique(x$yday)}
-
-	levs_lookup=vector("list", length(levs))
-	names(levs_lookup) <- levs
-	for(i in 1:length(levs)){
-		if(level=="monthly"){levs_lookup[[i]] <- which(x$mon == names(levs_lookup[i]))}
-		if(level=="daily"){levs_lookup[[i]] <- which(x$yday == names(levs_lookup[i]))}
+	if(level=="month"){
+    levs <- as.POSIXlt(x)$mon
+    ulevs <- unique(levs)
+    levs_lookup <- lapply(ulevs, function(x,y) which(y == x), levs)
+    names(levs_lookup) <- ulevs
+	}
+	if(level=="julian"){
+    levs <- as.POSIXlt(x)$yday
+    ulevs <- unique(levs)
+    levs_lookup <- lapply(ulevs, function(x,y) which(y == x), levs)
+    names(levs_lookup) <- ulevs
+	}
+  if(level=="day"){
+    levs <- format(x, format="%m%d")
+    ulevs <- unique(levs)
+    levs_lookup <- lapply(ulevs, function(x,y) which(y == x), levs)
+    names(levs_lookup) <- ulevs
+	}
+  
+  y.anom <- y*NaN
+  for(j in seq(levs_lookup)){     # for every time level
+    y.anom[levs_lookup[[j]],] <- t(t(as.matrix(y[levs_lookup[[j]],])) - apply(as.matrix(y[levs_lookup[[j]],]), 2, mean, na.rm=TRUE))
 	}
 
-	for(j in seq(levs)){     #for every time level
-    y[levs_lookup[[j]],] <- t(t(as.matrix(y[levs_lookup[[j]],])) - apply(as.matrix(y[levs_lookup[[j]],]), 2, mean, na.rm=TRUE))
-	}
-
-	y
+	y.anom
 
 }
 
