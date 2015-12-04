88d6f4fbcdd898f4180df2c2ec8dd221b1c929fd
marchtaylor <marc.taylor@ti.bund.de>
2015-12-04 13:22:56 +0100

    new Rd files

--- R/earthDist.R
+++ R/earthDist.R
@@ -5,9 +5,9 @@
 #' lon/lat positions (i.e. lon1 and lat1) can be a vector of positions to compare against
 #' the other lon/lat position (i.e. lon2 and lat2)
 #' 
-#' @param lon1 longitude 1 (in decimal degrees)
+#' @param lon1 Longitude 1 (in decimal degrees)
 #' @param lat1 Latitude 1 (in decimal degrees)
-#' @param lon2 longitude 2 (in decimal degrees)
+#' @param lon2 Longitude 2 (in decimal degrees)
 #' @param lat2 Latitude 2 (in decimal degrees)
 #' 
 #' @return Vector of distances (km)
