ea3b2450b7a6e41ed0caa3791e57d4e1a4c44073
marchtaylor <marchtaylor@gmail.com>
2020-12-04 07:55:55 +0100

    added stars and shadowtext funs

---
+++ R/shadowtext.R
@@ -0,0 +1,50 @@
+#' Add border to plotted text
+#'
+#' @param x,y numeric vectors of coordinates where the text labels should 
+#'   be written. If the length of x and y differs, the shorter one is recycled.
+#' @param labels a character vector or expression specifying the text to be 
+#'   written. An attempt is made to coerce other language objects 
+#'   (names and calls) to expressions, and vectors and other classed objects 
+#'   to character vectors by as.character. If labels is longer than x and y, 
+#'   the coordinates are recycled to the length of labels.
+#' @param col the color of the text.
+#' @param bg the color of the text border
+#' @param theta a sequence of angles be which to cycle around the text. 
+#'   (Default: \code{theta = seq(0, 2*pi, length.out = 50)}) 
+#' @param r the width of the border (fraction of character width).
+#' @param ... other pars passed to \code{\link[graphics]{text}}.
+#'
+#' @return lower level plotting command to add text to existing device.
+#' 
+#' @references  \url{https://stackoverflow.com/a/25632536/1199289}
+#' 
+#' @export
+#'
+#' @examples
+#' 
+#' plot(c(0,1), c(0,1), type="n", lwd=20, axes=FALSE, xlab="", ylab="")
+#' 
+#' rect(xleft = 0.5, xright = 1, ybottom = 0, ytop = 1, col=1)
+#' text(1/6, 1/6, "Test 1")
+#' shadowtext(2/6, 2/6, "Test 2", col='red', bg="blue")
+#' shadowtext(3/6, 3/6, "Test 3", cex=2)
+#' 
+#' # `r` controls the width of the border
+#' shadowtext(5/6, 5/6, "Test 4", col="black", bg="white", cex=4, r=0.2)
+#' 
+shadowtext <- function(x, y = NULL, labels, col = 'white', bg = 'black', 
+  theta = seq(0, 2*pi, length.out = 50), r = 0.1, ...){
+
+    xy <- xy.coords(x, y) # coordinates for text
+    xo <- r*strwidth('A') # character border width in plot units
+    yo <- r*strheight('A') # character border height in plot units
+
+    # draw background text with small shift in x and y in background colour
+    for (i in theta) {
+        text(x = xy$x + cos(i)*xo, y = xy$y + sin(i)*yo, labels = labels, 
+          col = bg, ...)
+    }
+    
+    # draw actual text in exact xy position in foreground colour
+    text(x = xy$x, y = xy$y, labels = labels, col = col, ...)
+}
---
+++ R/stars.R
@@ -0,0 +1,26 @@
+#' Return symbols for corresponding p-values
+#'
+#' @param x numeric vector of p-values
+#' @param breaks numeric vector of breaks (passed to \code{\link[base]{cut}})
+#' @param symbols character vector of text output corresponding to cut levels 
+#'
+#' @return character vector
+#' @export
+#'
+#' @examples
+#' 
+#' ctl <- c(4.17,5.58,5.18,6.11,4.50,4.61,5.17,4.53,5.33,5.14)
+#' trt <- c(4.81,4.17,4.41,3.59,5.87,3.83,6.03,4.89,4.32,4.69)
+#' group <- gl(2, 10, 20, labels = c("Ctl","Trt"))
+#' weight <- c(ctl, trt)
+#' lm.D9 <- lm(weight ~ group)
+#' summary(lm.D9) # summary table shows stars for signifcance levels
+#' Pvals <- summary(lm.D9)[[4]][,4] # extract p-values
+#' stars(x = Pvals) # reproduce stars
+#' 
+#' 
+stars <- function(x, breaks = c(0, 0.001, 0.01, 0.05, 0.1, 1), 
+  symbols = c("***", "**", "*", ".", "")){
+  siglev <- cut(x = x, breaks = breaks)
+  return(symbols[as.numeric(siglev)])
+}
