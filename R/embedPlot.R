441e6996af9161f5cae4e6bfee2e943b01bd68ad
Marc Taylor <marchtaylor@gmail.com>
2025-03-28 16:55:45 +0100

    embedPlot preserves all par()

--- R/embedPlot.R
+++ R/embedPlot.R
@@ -34,6 +34,25 @@
 #'     mtext("Elevation [m]", side = 1, line = 2)}), 
 #'   at = c(0.5, 0.95, 0.9, 0.95))
 #' 
+#' # addition of elevation scale and profile
+#' # Note that yyou may need to add a background box manually
+#' image(volcano, xlab = "X", ylab = "Y")
+#' embedPlot(
+#'   expr = expression({
+#'     imageScale(volcano, axis.pos = 1)
+#'     mtext("Elevation [m]", side = 1, line = 2)}), 
+#'   at = c(0.5, 0.95, 0.9, 0.95))
+#'   
+#' abline(v = 0.2, lty = 2, lwd = 2)
+#' polygon(x = c(0.4,0.96,0.96,0.4), y = c(0.01,0.01,0.52,0.52), col = "white")
+#' embedPlot(
+#'   expr = expression({
+#'     plot(x = seq(0, 1, length.out = nrow(volcano)), 
+#'       y = volcano[,round(ncol(volcano)*0.2)], xlab = "Y", ylab = "Elevation", bg = "white")
+#'   }), 
+#'   at = c(0.5, 0.95, 0.1, 0.5))
+#' box() # should be back at the full plot region
+#'   
 #' 
 #' # example mixing log scales, and demo of par changes
 #' set.seed(1)
@@ -64,14 +83,15 @@ embedPlot <- function(
     vec1}
   
   # Only write to par once for drawing insert plot: change back afterwards
-  plt <- par("plt")
+  op <- par(no.readonly = T) 
+  plt <- op$"plt"
   plt_space <- space_convert(at, plt)
   par(plt = plt_space, new = TRUE)
-  
+
   # evaluate expression
   eval(expr = expr)
   
-  par(plt = plt)
+  par(op)
 }
 
 
